c25428f295d8ba35879ab281c4423cc82035be0f
Jefferson Holbrook <holbrook002@gmail.com>
2016-03-23 16:40:28 -0400

    almost working solution using recursion

--- src/com/jholbrook/interviewquestions/PermutationIndexSearch.java
+++ src/com/jholbrook/interviewquestions/PermutationIndexSearch.java
@@ -1,8 +1,30 @@
 package com.jholbrook.interviewquestions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class PermutationIndexSearch {
 
+    private static List<Character> output = new ArrayList<>();
+
     static String findPermIndex(String str, int index){
+
+        int binSize = 1;
+        for(int i = 1; i <= str.length(); i++){
+            binSize = binSize * i;
+        }
+
+        System.out.println("bin size: " + binSize);
+
+        int bin = binSize/index;
+        output.add(str.charAt(bin));
+        if(bin != 1){
+            //recurse
+            System.out.println("gotta complete this");
+        } else{
+            return output.toString();
+        }
+
 
         return null;
     }
