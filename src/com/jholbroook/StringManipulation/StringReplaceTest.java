0f2ced7cac83939d3ae1d662397da3a59c1cff8a
Jefferson Holbrook <holbrook002@gmail.com>
2016-03-25 14:43:36 -0400

    StringReplace class and method fully functional.  Tests all pass for edge cases

---
+++ src/com/jholbroook/StringManipulation/StringReplaceTest.java
@@ -0,0 +1,33 @@
+package com.jholbroook.StringManipulation;
+
+//Test class for the StringReplace Class
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringReplaceTest {
+    @Test
+    public void testIfSpacesReplaced() throws Exception {
+        char[] testInput = ("This is a test      ").toCharArray();
+        //char[] expected = {'T', 'h', 'i', 's', '%', '2', '0', 'i', 's','%', '2', '0', 'a', '%', '2', '0', 't', 'e', 's', 't'};
+        char[] expected = ("This%20is%20a%20test").toCharArray();
+        assertArrayEquals("Method should replace all spaces with '%20'", expected,
+                StringReplace.replaceSpaces(testInput));
+    }
+    @Test
+    public void testStringReplaceForTrailingSpace() throws Exception {
+        char[] testInput = ("Test the edge case         ").toCharArray();
+        char[] expected = ("Test%20the%20edge%20case%20").toCharArray();
+        assertArrayEquals("Method handle trailing spaces properly", expected,
+                StringReplace.replaceSpaces(testInput));
+    }
+
+    @Test
+    public void testStrignReplaceHandleBlankInput() throws Exception{
+        char[] testInput = {};
+        char[] expected = {};
+        assertArrayEquals("StringReplace should accept a blank char[] and return an empty char[]",
+                expected, StringReplace.replaceSpaces(testInput));
+    }
+
+}
