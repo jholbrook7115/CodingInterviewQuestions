e0a0d451d85d2ffdbb4c82f0091407a934ee7d37
Jefferson Holbrook <holbrook002@gmail.com>
2016-04-05 13:43:44 -0400

    Added pointers to reference positions in the linked list

--- src/com/jholbrook/LinkedLists/PalindromeFinder.java
+++ src/com/jholbrook/LinkedLists/PalindromeFinder.java
@@ -9,17 +9,23 @@ package com.jholbrook.LinkedLists;
 
 public class PalindromeFinder {
 
+    private static NodeSinglyLL firstptr;
+    private static NodeSinglyLL midptr;
     public static boolean isPalindrome(NodeSinglyLL head){
+        firstptr = head;
+        midptr = head;
+
         int midIndex;
         while(head.next.next != null){
-
+            midptr = midptr.next;
+            head = midptr.next.next;
         }
-        boolean result = traverse(head, 0);
+        boolean result = traverse(firstptr, 0);
         return result;
     }
 
     private static boolean traverse(NodeSinglyLL node, int index){
-        
+
         return false;
     }
 }
