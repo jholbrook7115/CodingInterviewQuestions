a57c46ba006ebae930de014596d5045135fd5019
Jefferson Holbrook <holbrook002@gmail.com>
2016-04-06 15:05:50 -0400

    Added test to check that forward and backward traversal is correct

--- src/com/jholbrook/LinkedLists/NodeDoubleyLL.java
+++ src/com/jholbrook/LinkedLists/NodeDoubleyLL.java
@@ -2,4 +2,46 @@ package com.jholbrook.LinkedLists;
 
 
 public class NodeDoubleyLL {
+
+    public int data;
+    public NodeDoubleyLL next;
+    public NodeDoubleyLL prev;
+    public String str;
+    public char ch;
+
+    public NodeDoubleyLL(int data){
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+
+    public NodeDoubleyLL(char ch){
+
+        this.ch = ch;
+        this.next = null;
+        this.prev = null;
+    }
+
+    public void appendToTail(int data){
+        NodeDoubleyLL head = this;
+        NodeDoubleyLL newNode = new NodeDoubleyLL(data);
+
+        while(head.next != null){
+            head = head.next;
+        }
+        head.next = newNode;
+        newNode.prev = head;
+    }
+
+    public void appendToTail(char ch){
+        NodeDoubleyLL head = this;
+        NodeDoubleyLL newNode = new NodeDoubleyLL(ch);
+
+        while(head.next != null){
+            head = head.next;
+        }
+
+        head.next = newNode;
+        newNode.prev = head;
+    }
 }
---
+++ src/com/jholbrook/LinkedLists/NodeDoubleyLLTest.java
@@ -0,0 +1,59 @@
+package com.jholbrook.LinkedLists;
+
+//tests for java class NodeDoubleyLL
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class NodeDoubleyLLTest {
+
+    @Test
+    public void testInstantiateNode(){
+        NodeDoubleyLL head = new NodeDoubleyLL(1);
+
+        int expected = 1;
+        int actual = head.data;
+
+        assertEquals("newly created head node should have int value 1", expected, actual);
+    }
+
+    @Test
+    public void testAppendToTail(){
+        NodeDoubleyLL head = new NodeDoubleyLL(1);
+        head.appendToTail(2);
+        head.appendToTail(3);
+
+
+        int expected = 3;
+        while(head.next != null){
+            head = head.next;
+        }
+        int actual = head.data;
+
+        assertEquals("3rd node in the list shoudl have int value 3", expected, actual);
+    }
+
+    @Test
+    public void testTraversal(){
+        NodeDoubleyLL head = new NodeDoubleyLL(1);
+        head.appendToTail(2);
+        head.appendToTail(3);
+        head.appendToTail(4);
+        head.appendToTail(5);
+
+        int expected1 = 4;
+        for(int i = 0; i < 3; i++){
+            head = head.next;
+        }
+        int actual1 = head.data;
+
+        assertEquals("third element in the list should have int value 3", expected1, actual1);
+
+        int expected2 = 2;
+        for(int i = 0; i < 2; i++){
+            head = head.prev;
+        }
+        int actual2 = head.data;
+        assertEquals("second element in the list should have int value 2", expected2, actual2);
+    }
+
+}
